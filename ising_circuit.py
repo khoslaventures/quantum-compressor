a67dac48d822ae60f621c7c1ec035c5dfeac3914
Akash Khosla <me@akashkhosla.com>
2019-05-15 13:31:11 -0700

    refactor: make ising_circuit use a class instead

--- ising_circuit.py
+++ ising_circuit.py
@@ -43,75 +43,77 @@ TODO(akash): Provide example of circuit with given inputs in docstring
 """
 
 
-def ising_circuit(n, J, J_max, L, T):
-    """Generate a circuit for compressed quantum simulation of the Ising model
-    in the 1D case using a 1D non-circular arrangement of qubits, which will
-    allow the hamiltonian to be compressed via matchgates.
+class IsingCircuit:
+    def __init__(self, n, J, J_max, L, T):
+        """Generate a circuit for compressed quantum simulation of the Ising
+        model in the 1D case using a 1D non-circular arrangement of qubits,
+        which will allow the hamiltonian to be compressed via matchgates.
 
-    Args:
-        n: The num of qubits to be evolved to a state J
-        J: State of the magnetization, ratio of interaction strength and
-           external field strength
-        J_max: The maximium J value for evolution
-        L: The number of steps
-        T: The time of evolution
+        Args:
+            n: The num of qubits to be evolved to a state J
+            J: State of the magnetization, ratio of interaction strength and
+               external field strength
+            J_max: The maximium J value for evolution
+            L: The number of steps
+            T: The time of evolution
 
-    Returns: circuit for compressed Ising simulation
+        Returns: circuit for compressed Ising simulation
 
-    For L = 2000 and T = 100, the circuit should produce very accurate values.
-    However, it makes for very slow runtimes, so we've been using L = 200 and
-    T = 10 which Kraus claimed to produce decent enough results.
-    """
-    dt = T / (L + 1)
-    m = int(np.log2(n)) + 1
-    qubits = cirq.LineQubit.range(m)
-    circuit = cirq.Circuit()
+        For L = 2000 and T = 100, the circuit should produce very accurate
+        values.  However, it makes for very slow runtimes, so we've been using
+        L = 200 and T = 10 which Kraus claimed to produce decent enough
+        results.
+        """
+        self.dt = T / (L + 1)
+        self.m = int(np.log2(n)) + 1
+        self.qubits = cirq.LineQubit.range(self.m)
+        self.circuit = cirq.Circuit()
 
-    # Initial states - H and S gates are for |+>(Y) state, bit flip is for
-    # mixed state
-    circuit.append([cirq.H(qubits[m - 1])])
-    circuit.append([cirq.S(qubits[m - 1])])
+        # Initial states - H and S gates are for |+>(Y) state, bit flip is for
+        # mixed state
+        self.circuit.append([cirq.H(self.qubits[self.m - 1])])
+        self.circuit.append([cirq.S(self.qubits[self.m - 1])])
 
-    bit_flip = cirq.BitFlipChannel(0.5)
-    for i in range(0, m - 1):
-        circuit.append([bit_flip.on(qubits[i])])
+        bit_flip = cirq.BitFlipChannel(0.5)
+        for i in range(0, self.m - 1):
+            self.circuit.append([bit_flip.on(self.qubits[i])])
 
-    # LJ determines the number of adiabatic steps to take
-    LJ = int(J * L / J_max)
-    for l in range(0, LJ):
-        Jl = J_max * l / L
+        # LJ determines the number of adiabatic steps to take
+        self.LJ = int(J * L / J_max)
+        for l in range(0, self.LJ):
+            Jl = J_max * l / L
 
-        # Rotate qubit m
-        R0l = R0(-4 * dt)
-        circuit.append([R0l.on(qubits[m-1])])
+            # Rotate qubit m
+            R0l = R0(-4 * self.dt)
+            self.circuit.append([R0l.on(self.qubits[self.m-1])])
 
-        # shift qubit states up so the rotation matrix Rl acts on the states
-        # correctly
-        shiftu = SHIFTU(m)
-        circuit.append(shiftu(*qubits))
+            # shift qubit states up so the rotation matrix Rl acts on the
+            # states correctly
+            shiftu = SHIFTU(self.m)
+            self.circuit.append(shiftu(*self.qubits))
 
-        # application of Rl, a rotation matrix on the whole state
-        # phi_l is the angle
-        # We apply the rotation gate (r) to the pair of states we care about
-        # (they are on qubit m after shifting)
-        phi_l = 2 * Jl * dt
-        r = cirq.SingleQubitMatrixGate(
-            np.array([[np.cos(phi_l), -np.sin(phi_l)], [np.sin(phi_l),
-                                                        np.cos(phi_l)]]))
-        circuit.append([r.on(qubits[m-1])])
-        # We then apply a controlled inverse of (r), with all the other qubits
-        # as controls This effectively gives us our desired Rl on the
-        # wavefunction
-        controls = qubits[0:m-1]
-        Cr = cirq.ControlledGate(sub_gate=(r**-1), control_qubits=controls)
-        circuit.append(Cr.on(qubits[m - 1]))
+            # application of Rl, a rotation matrix on the whole state
+            # phi_l is the angle
+            # We apply the rotation gate (r) to the pair of states we care
+            # about (they are on qubit m after shifting)
+            phi_l = 2 * Jl * self.dt
+            r = cirq.SingleQubitMatrixGate(
+                np.array([[np.cos(phi_l), -np.sin(phi_l)], [np.sin(phi_l),
+                                                            np.cos(phi_l)]]))
+            self.circuit.append([r.on(self.qubits[self.m-1])])
+            # We then apply a controlled inverse of (r), with all the other
+            # qubits as controls This effectively gives us our desired Rl on
+            # the wavefunction
+            controls = self.qubits[0:self.m-1]
+            Cr = cirq.ControlledGate(sub_gate=(r**-1), control_qubits=controls)
+            self.circuit.append(Cr.on(self.qubits[self.m - 1]))
 
-        # Shift back down for R0 to work correctly
-        shiftd = SHIFTD(m)
-        circuit.append(shiftd(*qubits))
+            # Shift back down for R0 to work correctly
+            shiftd = SHIFTD(self.m)
+            self.circuit.append(shiftd(*self.qubits))
 
-    # these are applied for measurement of Y on qubit m
-    circuit.append([cirq.S(qubits[m - 1])**-1])
-    circuit.append([cirq.H(qubits[m - 1])])
-    circuit.append([cirq.measure(qubits[m - 1], key='x')])
-    return circuit
+        # these are applied for measurement of Y on qubit self.m
+        self.circuit.append([cirq.S(self.qubits[self.m - 1])**-1])
+        self.circuit.append([cirq.H(self.qubits[self.m - 1])])
+        self.circuit.append([cirq.measure(self.qubits[self.m - 1], key='x')])
+        return self.circuit
